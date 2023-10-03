fd0edec0bd9abf965ca52360b8692a51a30e9779
Azrubael <i0638464000@gmail.com>
2023-10-03 12:30:01 +0300

    Step #05. Renamed files oop44.py, oop45.py, oop47.py and a new file 'oop49.py'.

---
+++ init_del.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+class Point:
+    color = 'red'
+    circle = 2
+    
+    def __init__(self, x=0, y=0):
+        print ("Call of __init__")
+        self.x = x
+        self.y = y
+        
+    def __del__(self):
+    """This method is calling automatically at the end of the scrypt"""
+        print("Deleting an instance: " + str(self))
+    
+    def set_coords(self, x, y):
+        print("Call of set_coords")
+        self.x = x
+        self.y = y
+        
+    def get_coords(self):
+        print("Call of get_coords")
+        return self.x, self.y
+        
+
+pt1 = Point()
+print(pt1.__dict__)
+pt1.set_coords(10, 20)
+print(pt1.__dict__)
+print()
+
+pt2 = Point(19, 37)
+print(pt2.__dict__)
+# print(pt2.__del__)
